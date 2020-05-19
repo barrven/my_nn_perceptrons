13fb69cb4f92590104797a1922166604d848af98
Barrington <barrven@users.noreply.github.com>
2020-05-19 04:48:44 -0400

    Started to flesh out the basic structure of the AndPerceptron class.

--- src/nn_perceptrons/Main.java
+++ src/nn_perceptrons/Main.java
@@ -4,6 +4,18 @@ import nn_perceptrons.classes.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        //training data
+        double[][] tInput = new double[4][2];
+        tInput[0][0] = 0;
+        tInput[0][1] = 0;
+        tInput[1][0] = 0;
+        tInput[1][1] = 1;
+        tInput[2][0] = 1;
+        tInput[2][1] = 0;
+        tInput[3][0] = 1;
+        tInput[3][1] = 1;
+        int[] cAnswer = {0,0,0,1};
+
+        AndPerceptron a = new AndPerceptron(tInput, cAnswer, 0.1);
     }
 }
